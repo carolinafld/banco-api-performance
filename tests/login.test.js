14f2794c00e354d8000c9bf6bb1dbb55225b35ef
Carolina Friedrich Lobo D Avila <137728605+carolinafld@users.noreply.github.com>
2026-05-26 23:08:14 -0400

    Adicionando a função de helper para obtenção de token e criando o teste de transferência

--- tests/login.test.js
+++ tests/login.test.js
@@ -1,6 +1,6 @@
-import http from 'k6/http'
-import { sleep, check } from 'k6'
-const postLogin = JSON.parse(open('../fixtures/postLogin.json'))
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+const postLogin = JSON.parse(open('../fixtures/postLogin.json'));
 
 export const options = {
     stages: [
@@ -12,25 +12,25 @@ export const options = {
       http_req_duration: ['p(90)<3000', 'max<5000'],
       http_req_failed: ['rate<0.01']
     }
-}
+};
 
 export default function () {
-    const url = 'http://localhost:3000/login'
+    const url = 'http://localhost:3000/login';
     
-    const payload = JSON.stringify(postLogin)
+    const payload = JSON.stringify(postLogin);
 
     const params = {
       headers: {
         'Content-Type': 'application/json'
       }
-    }
+    };
 
-    const res = http.post(url, payload, params)
+    const res = http.post(url, payload, params);
 
     check(res, {
       'Validar que o Status é 200': (r) => r.status === 200,
       'Validar que o Token é string': (r) => typeof(r.json().token) == 'string'
-    })
+    });
 
-    sleep(1)
+    sleep(1);
 }
